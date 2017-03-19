6705db8bddb7948a0d1fdc0f5e00662a47998937
un-knight <yjx.underworld@gmail.com>
2017-03-19 11:00:39 +0800

    finish factory pattern

--- factory_pattern/gameboard.py
+++ factory_pattern/gameboard.py
@@ -22,8 +22,8 @@ else:
 
 
 class AbstractBoard:
-  def __init__(self, rows, colums):
-    self.board = [[None for _ in range(colums) for _ in range(rows)]]
+  def __init__(self, rows, columns):
+    self.board = [[None for _ in range(columns)] for _ in range(rows)]
     self.populate_board()
 
   def populate_board(self):
@@ -34,6 +34,9 @@ class AbstractBoard:
     for y, row in enumerate(self.board):
       for x, piece in enumerate(row):
         square = console(piece, BLACK if (y + x) % 2 else WHITE)
+        squares.append(square)
+      squares.append("\n")
+    return "".join(squares)
 
 class CheckersBoard(AbstractBoard):
   def __init__(self):
@@ -52,6 +55,43 @@ class CheckersBoard(AbstractBoard):
     self.board = [list(itertools.islice(
       itertools.cycle(squares), 0, len(rows))) for squares in rows]
 
+
+class ChessBoard(AbstractBoard):
+  def __init__(self):
+    super().__init__(8, 8)
+
+  def populate_board(self):
+    for row, color in ((0, BLACK), (7, WHITE)):
+      for columns, kind in (((0, 7), ROOK), ((1, 6), KNIGHT),
+        ((2, 5), BISHOP), ((3,), QUEEN), ((4,), KING)):
+        for column in columns:
+          self.board[row][column] = create_piece(kind, color)
+    for column in range(8):
+      for row, color in ((1, BLACK), (6, WHITE)):
+        self.board[row][column] = create_piece(PAWN, color)
+
+
+def create_piece(kind, color):
+  color = "White" if color == WHITE else "Black"
+  name = {DRAUGHT: "Draught", PAWN: "ChessPawn", ROOK: "ChessRook",
+          KNIGHT: "ChessKnight", BISHOP: "ChessBishop",
+          KING: "ChessKing", QUEEN: "ChessQueen"}[kind]
+  return globals()[color + name]()
+
+
+class Piece(str):
+  __slots__ = ()
+
+
+for code in itertools.chain((0x26C0, 0x26C2), range(0x2654, 0x2660)):
+  char = chr(code)
+  name = unicodedata.name(char).title().replace(" ", "")
+  if name.endswith("sMan"):
+    name = name[:-4]
+  new = (lambda char: lambda Class: Piece.__new__(Class, char))(char)
+  new.__name__ = "__new__"
+  Class = type(name, (Piece,), dict(__slots__=(), __new__=new))
+  globals()[name] = Class
 
 
 def main():
